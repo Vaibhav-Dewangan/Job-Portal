deedb26c574161f49eb8cf70be94cb296059c8ab
Vaibhav-Dewangan <vaibhavdewangan28@gmail.com>
2025-04-14 23:56:57 +0530

    favicon icon added

--- Job-Portal/resources/views/layouts/app.blade.php
+++ Job-Portal/resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="{{ asset('favicon.png') }}" type="image/png">
     <title>{{config('app.name')}}</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
     @vite(['resources/css/app.css', 'resources/js/app.js',])
